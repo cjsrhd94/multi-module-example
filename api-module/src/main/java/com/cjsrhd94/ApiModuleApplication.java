061ed94b0f6b0ad2f65c9986fa2108d5a3ef12ab
cjsrhd94 <cjsrhd94@gmail.com>
2023-07-10 13:45:45 +0900

    config: 멀티 모듈 초기 셋팅

--- api-module/src/main/java/com/cjsrhd94/ApiModuleApplication.java
+++ api-module/src/main/java/com/cjsrhd94/ApiModuleApplication.java
@@ -1,13 +1,11 @@
-package com.multimodule.demo;
+package com.cjsrhd94;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class ApiModuleApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiModuleApplication.class, args);
 	}
-
 }
